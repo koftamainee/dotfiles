8d278d2cc23b0dedcbfe16ab5c74fba315dd644c
koftamainee <koftamainee@gmail.com>
2025-10-28 03:20:42 +0300

    Backup on Tue Oct 28 03:20:42 AM MSK 2025

--- hypr/scripts/PowerModes.sh
+++ hypr/scripts/PowerModes.sh
@@ -5,24 +5,28 @@ current_mode=$(powerprofilesctl get)
 
 # Cycle to the next mode and set icon
 case "$current_mode" in
+    "performance")
+        next_mode="balanced"
+        icon=""
+        xrandr --output eDP-1 --mode 2560x1600 --rate 120  # Normal refresh rate
+        ;;
     "balanced")
+        next_mode="power-saver"
+        icon=""
+        xrandr --output eDP-1 --mode 2560x1600 --rate 60   # Lower refresh rate for power saving
+        ;;
+    "power-saver")
         next_mode="performance"
         icon=""
         xrandr --output eDP-1 --mode 2560x1600 --rate 120  # Restore normal refresh rate
         ;;
-    "performance")
-        next_mode="balanced"
-        icon=""
-        xrandr --output eDP-1 --mode 2560x1600 --rate 120 # Set to 60Hz for power saving
-        ;;
     *)
         next_mode="balanced"
         icon=""
-        xrandr --output eDP-1 --mode 2560x1600 --rate 120  # Restore normal refresh rate
+        xrandr --output eDP-1 --mode 2560x1600 --rate 120
         ;;
 esac
 
 # Set the new mode and send notification
 powerprofilesctl set "$next_mode"
 notify-send -r 9999 "Power Mode" "$icon  $next_mode mode activated" -u low
-
